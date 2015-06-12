c42164e2384c2df587678f83f3daa9c233290b4c
Andreas Pauley <apauley+github@gmail.com>
2015-06-12 23:29:01 +0200

    Exit when window closes

--- src/MainGTK.hs
+++ src/MainGTK.hs
@@ -1,8 +1,10 @@
 import Graphics.UI.Gtk
+import Control.Monad.Trans(liftIO)
 
 main :: IO ()
 main = do
    _ <- initGUI
    window <- windowNew
+   _ <- window `on` deleteEvent $ liftIO mainQuit >> return False
    widgetShowAll window
    mainGUI
