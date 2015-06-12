32047754297fb6acd96da5f897dc47c5d927e67e
Andreas Pauley <apauley+github@gmail.com>
2015-06-12 23:19:24 +0200

    print directory contents

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,7 @@
+import System.Directory
+
 main :: IO ()
 main = do
+  files <- getDirectoryContents "."
+  print files
   putStrLn "This program does not do anything useful. Yet. Time to start working..."
